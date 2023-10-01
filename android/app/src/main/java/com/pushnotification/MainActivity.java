f9275edea3fc7d8c7af8b9332158120c8ce1235a
Manish Kumar <manish.kumar@tradesocio.com>
2023-10-01 13:59:16 +0530

    sound

--- android/app/src/main/java/com/pushnotification/MainActivity.java
+++ android/app/src/main/java/com/pushnotification/MainActivity.java
@@ -1,4 +1,15 @@
 package com.pushnotification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.ContentResolver;
+import android.media.AudioAttributes;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+
+import androidx.core.app.NotificationCompat;
+
+import com.facebook.react.ReactActivity;
 
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
@@ -14,6 +25,41 @@ public class MainActivity extends ReactActivity {
   @Override
   protected String getMainComponentName() {
     return "PushNotification";
+  }
+
+  @Override
+  protected  void onCreate(Bundle savedInstanceState){
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+      NotificationChannel notificationChannel = new NotificationChannel("sound_channel1", "Notification", NotificationManager.IMPORTANCE_HIGH);
+      notificationChannel.setShowBadge(true);
+      notificationChannel.setDescription("");
+      AudioAttributes att = new AudioAttributes.Builder()
+              .setUsage(AudioAttributes.USAGE_NOTIFICATION)
+              .setContentType(AudioAttributes.CONTENT_TYPE_SPEECH)
+              .build();
+      notificationChannel.setSound(Uri.parse(ContentResolver.SCHEME_ANDROID_RESOURCE + "://" + getPackageName() + "/raw/custom"), att);
+      notificationChannel.enableVibration(true);
+      notificationChannel.setVibrationPattern(new long[]{400, 400});
+      notificationChannel.setLockscreenVisibility(NotificationCompat.VISIBILITY_PUBLIC);
+      NotificationManager manager = getSystemService(NotificationManager.class);
+      manager.createNotificationChannel(notificationChannel);
+    }
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+      NotificationChannel notificationChannel = new NotificationChannel("sound_channel2", "Notification", NotificationManager.IMPORTANCE_HIGH);
+      notificationChannel.setShowBadge(true);
+      notificationChannel.setDescription("");
+      AudioAttributes att = new AudioAttributes.Builder()
+              .setUsage(AudioAttributes.USAGE_NOTIFICATION)
+              .setContentType(AudioAttributes.CONTENT_TYPE_SPEECH)
+              .build();
+      notificationChannel.setSound(Uri.parse(ContentResolver.SCHEME_ANDROID_RESOURCE + "://" + getPackageName() + "/raw/custom"), att);
+      notificationChannel.enableVibration(true);
+      notificationChannel.setVibrationPattern(new long[]{400, 400});
+      notificationChannel.setLockscreenVisibility(NotificationCompat.VISIBILITY_PUBLIC);
+      NotificationManager manager = getSystemService(NotificationManager.class);
+      manager.createNotificationChannel(notificationChannel);
+    }
+    super.onCreate(savedInstanceState);
   }
 
   /**
